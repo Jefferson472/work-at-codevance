0dac3274c6c72c8c925e1bb8206a9dd5226c5bf1
Jefferson Miranda <jefferson472@hotmail.com>
2023-02-21 12:05:19 -0300

    test: payment list view

--- src/apps/pages/tests/tests_urls.py
+++ src/apps/pages/tests/tests_urls.py
@@ -2,7 +2,11 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 
 
-class TestCandidateUrls(TestCase):
+class TestUrls(TestCase):
     def test_home_page_url(self):
         self.assertEqual(reverse('home_page'), '/')
         self.assertEqual(resolve('/').view_name, 'home_page')
+
+    def test_payments_url(self):
+        self.assertEqual(reverse('payments_list'), '/payments/')
+        self.assertEqual(resolve('/payments/').view_name, 'payments_list')
--- src/apps/pages/tests/tests_views.py
+++ src/apps/pages/tests/tests_views.py
@@ -15,3 +15,17 @@ class TestHomePageViews(TestCase):
         self.client.force_login(self.user)
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+
+class TestPaymentListView(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create(email='user@test.com')
+
+    def test_payment_list_view_anonymous_302(self):
+        response = self.client.get('/payments/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_payment_list_view_authenticated_200(self):
+        self.client.force_login(self.user)
+        response = self.client.get('/payments/')
+        self.assertEqual(response.status_code, 200)
