bc352356ee9835502b4eb765c1de0ce3ddadea2c
Jefferson Miranda <jefferson472@hotmail.com>
2023-02-21 09:49:09 -0300

    test: models payment

--- src/apps/payment/tests.py
+++ src/apps/payment/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from apps.payment.models import Payment
+from apps.user_profile.models import Supplier
+
+
+class PaymentModelTestCase(TestCase):
+    def setUp(self):
+        self.supplier = Supplier.objects.create(name='Test Supplier')
+        self.payment = Payment.objects.create(
+            supplier=self.supplier,
+            description='Test Payment',
+            value=100.00,
+            date_due='2023-12-31',
+        )
+
+    def test_payment_creation(self):
+        self.assertEqual(Payment.objects.count(), 1)
+
+    def test_payment_defaults(self):
+        self.assertTrue(self.payment.is_active)
+        self.assertIsNotNone(self.payment.created)
