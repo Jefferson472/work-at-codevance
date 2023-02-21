791c12f029e7179ee2c12eac191f2b9390073ff0
Jefferson Miranda <jefferson472@hotmail.com>
2023-02-21 08:09:54 -0300

    feat: add model payments

--- src/apps/payment/models.py
+++ src/apps/payment/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from apps.user_profile.models import Supplier
+
+
+class Payment(models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True, related_name='supplier')
+    description = models.CharField(max_length=255, blank=True, null=True)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    date_due = models.DateField()
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.supplier} - {self.value} - {self.date_due}'
