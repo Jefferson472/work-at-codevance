2ebbc99e06897fa8d6998dc3e8a4925b57add43a
Jefferson Miranda <jefferson472@hotmail.com>
2023-02-21 10:19:52 -0300

    test: add test views homepage

---
+++ src/apps/pages/tests/tests_views.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from apps.core.models import CustomUser
+
+
+class TestHomePageViews(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create(email='user@test.com')
+
+    def test_home_page_view_anonymous_302(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_home_page_view_authenticated_200(self):
+        self.client.force_login(self.user)
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
