4d4c25a5fbb0a7f6f64163cbfc5ed5809c20127f
Jefferson Miranda <jefferson472@hotmail.com>
2023-02-21 09:47:29 -0300

    fix: rename requester to operator, payment to request

--- src/apps/antecipation/models/Antecipation.py
+++ src/apps/antecipation/models/Antecipation.py
@@ -5,11 +5,11 @@ from apps.antecipation.models import RequestAntecipation
 
 
 class Antecipation(models.Model):
-    requester = models.OneToOneField(Operator, on_delete=models.SET_NULL, null=True, related_name='operator')
-    payment = models.OneToOneField(RequestAntecipation, on_delete=models.SET_NULL, null=True, related_name='request')
+    operator = models.OneToOneField(Operator, on_delete=models.SET_NULL, null=True, related_name='operator')
+    request_antecipation = models.OneToOneField(RequestAntecipation, on_delete=models.SET_NULL, null=True, related_name='request')
     fee = models.DecimalField(max_digits=10, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.payment} - {self.fee}'
+        return f'{self.fee}'
