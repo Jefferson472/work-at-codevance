a269ccccbb6f42d6004051bf25815751448ab885
Jefferson Miranda <jefferson472@hotmail.com>
2023-02-21 09:48:05 -0300

    test: models antecipation

--- src/apps/antecipation/tests.py
+++ src/apps/antecipation/tests.py
@@ -1,3 +1,57 @@
+import random
+import string
+
 from django.test import TestCase
 
-# Create your tests here.
+from apps.antecipation.models import Antecipation, LogTransactions, RequestAntecipation
+from apps.core.models import CustomUser
+from apps.payment.models import Payment
+from apps.user_profile.models import Operator
+
+
+class BaseTestCase(TestCase):
+    def setUp(self):
+        random_email = ''.join(random.choice(string.ascii_letters) for _ in range(10)) + '@test.com'
+        self.user_operator = CustomUser.objects.create(email=random_email)
+        self.operator = Operator.objects.create(
+            user=self.user_operator,
+            name='Operator',
+        )
+        self.payment = Payment.objects.create(
+            value=1000,
+            date_due='2023-02-20'
+        )
+        self.request_antecipation = RequestAntecipation.objects.create(
+            payment=self.payment,
+            requester=self.user_operator
+        )
+
+
+class RequestAntecipationTestCase(BaseTestCase):
+    def test_request_antecipation_str(self):
+        self.assertEqual(str(self.request_antecipation), f'{self.payment} - Aguardando avaliação')
+
+    def test_request_antecipation_status_choices(self):
+        self.assertEqual(self.request_antecipation.get_status_display(), 'Aguardando avaliação')
+
+
+class AntecipationModelTestCase(BaseTestCase):
+    def test_antecipation_creation(self):
+        Antecipation.objects.create(
+            operator=self.operator,
+            request_antecipation=self.request_antecipation,
+            fee=5.00
+        )
+        self.assertEqual(Antecipation.objects.count(), 1)
+
+
+class LogTransactionsModelTestCase(BaseTestCase):
+    def test_log_transactions_creation(self):
+        LogTransactions.objects.create(
+            operator=self.operator,
+            request=self.request_antecipation,
+            status_after="approved",
+            value_before=100.0,
+            value_after=90.0
+        )
+        self.assertEqual(LogTransactions.objects.count(), 1)
