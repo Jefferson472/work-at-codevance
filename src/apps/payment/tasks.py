0a89bc95a67c3ad53cc271bc34fb09daa57b38e8
Jefferson Miranda <jefferson472@hotmail.com>
2023-02-22 15:42:31 -0300

    feat: add metho to check payment valid due date

---
+++ src/apps/payment/tasks.py
@@ -0,0 +1,11 @@
+from django.utils import timezone
+
+from apps.payment.models import Payment
+
+
+def check_payment_due_date():
+    payments = Payment.objects.filter(is_active=True)
+    for payment in payments:
+        if timezone.now().date() > payment.date_due:
+            payment.is_active = False
+    Payment.objects.bulk_update(payments, ['is_active'])
