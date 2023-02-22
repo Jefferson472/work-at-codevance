5123f1d722dcac8c50e60eab3cc9c8c21d1f0726
Jefferson Miranda <jefferson472@hotmail.com>
2023-02-22 07:10:12 -0300

    hotfix: add created field in Logs

--- src/apps/antecipation/models/LogTransactions.py
+++ src/apps/antecipation/models/LogTransactions.py
@@ -10,3 +10,4 @@ class LogTransactions(models.Model):
     status_after = models.CharField(max_length=20)
     value_before = models.DecimalField(max_digits=10, decimal_places=2)
     value_after = models.DecimalField(max_digits=10, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
