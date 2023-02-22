9ec680c064c08fd635bfa9917b081643cb348741
Jefferson Miranda <jefferson472@hotmail.com>
2023-02-22 06:28:24 -0300

    fix: del default date migration

--- src/apps/antecipation/migrations/0005_requestantecipation_request_date.py
+++ src/apps/antecipation/migrations/0005_requestantecipation_request_date.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='requestantecipation',
             name='request_date',
-            field=models.DateField(default=''),
+            field=models.DateField(),
             preserve_default=False,
         ),
     ]
