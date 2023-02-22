eacd871d69d47a1f2fad7325ee830d269b21330b
Jefferson Miranda <jefferson472@hotmail.com>
2023-02-22 10:35:29 -0300

    hotfix: move fee from antecipation to request model

--- src/apps/antecipation/models/RequestAntecipation.py
+++ src/apps/antecipation/models/RequestAntecipation.py
@@ -14,6 +14,7 @@ class RequestAntecipation(models.Model):
     payment = models.OneToOneField(Payment, on_delete=models.SET_NULL, null=True, related_name='anticipation')
     requester = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, related_name='requester')
     request_date = models.DateField()
+    fee = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='0')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
