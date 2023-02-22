eb9f7d289e58ea8e90863d0121d1f118b30d22dd
Jefferson Miranda <jefferson472@hotmail.com>
2023-02-22 15:29:11 -0300

    feat: add log type

--- src/apps/antecipation/models/LogTransactions.py
+++ src/apps/antecipation/models/LogTransactions.py
@@ -1,12 +1,19 @@
 from django.db import models
 
-from apps.user_profile.models import Operator
+from apps.core.models import CustomUser
 from apps.antecipation.models import RequestAntecipation
 
 
 class LogTransactions(models.Model):
-    operator = models.ForeignKey(Operator, on_delete=models.SET_NULL, null=True)
-    request_antecipation = models.OneToOneField(RequestAntecipation, on_delete=models.SET_NULL, null=True)
+    TRANSACTION_TYPE = (
+        ('0', 'Request'),
+        ('1', 'Approval'),
+        ('2', 'Disapproval'),
+    )
+
+    requester = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    request_antecipation = models.ForeignKey(RequestAntecipation, on_delete=models.SET_NULL, null=True)
+    transaction_type = models.CharField(max_length=1, choices=TRANSACTION_TYPE)
     status_after = models.CharField(max_length=20)
     value_before = models.DecimalField(max_digits=10, decimal_places=2)
     value_after = models.DecimalField(max_digits=10, decimal_places=2)
