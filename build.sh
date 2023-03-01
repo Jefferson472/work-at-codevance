063f5750b4d65c7f606a4854ef98b03f29d667c0
Jefferson Miranda <jefferson472@hotmail.com>
2023-03-01 10:08:25 -0300

    build: remove seed after first deploy

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 python ./src/manage.py collectstatic --no-input
 python ./src/manage.py migrate --settings=setup.settings.prod
 
-python ./src/manage.py shell < src/seed.py --settings=setup.settings.prod
+# python ./src/manage.py shell < src/seed.py --settings=setup.settings.prod
